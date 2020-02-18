c6a9d0e7647b827545c34aca6c4df54758189c63
Andrew Sun <asun@redhat.com>
2020-02-18 11:48:23 -0500

    Revert "this should break it"
    This reverts commit 5b7aa4b5d22190e239fb22f22117af72315625d8.

--- go-controller/pkg/cluster/gateway_init.go
+++ go-controller/pkg/cluster/gateway_init.go
@@ -29,7 +29,7 @@ func bridgedGatewayNodeSetup(nodeName, bridgeName, bridgeInterface string, syncB
 		var err error
 
 		stdout, stderr, err := util.RunOVSVsctl("set", "bridge",
-			bridgeName, "other-config:hwaddr="+macAddress, "datapath_type=netdev")
+			bridgeName, "other-config:hwaddr="+macAddress)
 		if err != nil {
 			return "", "", fmt.Errorf("Failed to set bridge, stdout: %q, stderr: %q, "+
 				"error: %v", stdout, stderr, err)
