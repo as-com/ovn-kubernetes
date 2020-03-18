61a9f457badb1a2b5aa0269107a16727666bfa83
Andrew Sun <asun@redhat.com>
2020-03-18 17:56:27 -0400

    aaaaaaa

---
+++ test/e2e/e2e_test.go
@@ -0,0 +1,95 @@
+package e2e_test
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo"
+
+	// . "github.com/onsi/gomega"
+
+	// . "github.com/ovn-org/ovn-kubernetes/test/e2e"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/kubernetes/test/e2e/framework"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	e2epod "k8s.io/kubernetes/test/e2e/framework/pod"
+)
+
+// checkConnectivityToHost launches a pod to test connectivity to the specified
+// host. An error will be returned if the host is not reachable from the pod.
+//
+// An empty nodeName will use the schedule to choose where the pod is executed.
+func checkConnectivityToHost(f *framework.Framework, nodeName, podName, host string, port, timeout int) error {
+	contName := fmt.Sprintf("%s-container", podName)
+
+	command := []string{
+		"nc",
+		"-vz",
+		"-w", strconv.Itoa(timeout),
+		host,
+		strconv.Itoa(port),
+	}
+
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: podName,
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:    contName,
+					Image:   framework.AgnHostImage,
+					Command: command,
+				},
+			},
+			NodeName:      nodeName,
+			RestartPolicy: v1.RestartPolicyNever,
+		},
+	}
+	podClient := f.ClientSet.CoreV1().Pods(f.Namespace.Name)
+	_, err := podClient.Create(pod)
+	if err != nil {
+		return err
+	}
+	err = e2epod.WaitForPodSuccessInNamespace(f.ClientSet, podName, f.Namespace.Name)
+
+	if err != nil {
+		logs, logErr := e2epod.GetPodLogs(f.ClientSet, f.Namespace.Name, pod.Name, contName)
+		if logErr != nil {
+			framework.Logf("Warning: Failed to get logs from pod %q: %v", pod.Name, logErr)
+		} else {
+			framework.Logf("pod %s/%s logs:\n%s", f.Namespace.Name, pod.Name, logs)
+		}
+	}
+
+	return err
+}
+
+var _ = Describe("E2e", func() {
+	var svcname = "nettest"
+
+	f := framework.NewDefaultFramework(svcname)
+
+	ginkgo.BeforeEach(func() {
+		// Assert basic external connectivity.
+		// Since this is not really a test of kubernetes in any way, we
+		// leave it as a pre-test assertion, rather than a Ginko test.
+		ginkgo.By("Executing a successful http request from the external internet")
+		resp, err := http.Get("http://google.com")
+		if err != nil {
+			framework.Failf("Unable to connect/talk to the internet: %v", err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			framework.Failf("Unexpected error code, expected 200, got, %v (%v)", resp.StatusCode, resp)
+		}
+	})
+
+	ginkgo.It("should provide Internet connection for containers [Feature:Networking-IPv4]", func() {
+		ginkgo.By("Running container which tries to connect to 8.8.8.8")
+		framework.ExpectNoError(
+			checkConnectivityToHost(f, "", "connectivity-test", "8.8.8.8", 53, 30))
+	})
+})
